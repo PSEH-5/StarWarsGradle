e26ac1e44e3150851f324c70868cd8bde66a6aeb
PSEH-7 <PSEH-5@sapient.com>
2020-03-07 14:23:49 +0530

    First Commit

---
+++ src/main/java/com/sapient/test/StarWarsGradle/StarWarsGradleApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.test.StarWarsGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsGradleApplication.class, args);
+	}
+
+}
